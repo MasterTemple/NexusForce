c33fac5c1a8f9eb8424bdb60380bde623b3038d2
MasterTemple <dgmastertemple@protonmail.com>
2021-04-08 16:23:23 -0700

    added more info

--- functions/item.js
+++ functions/item.js
@@ -9,7 +9,7 @@ module.exports = {
         const item = require(`./../output/objects/${Math.floor(itemID/256)}/${itemID}.json`);
         const {emojis} = require('./../config.json');
 
-        console.log(item)
+        //console.log(item)
 
         // message.channel.send(`\`\`\`${JSON.stringify(item,null,2)}\`\`\``)
 
@@ -43,7 +43,27 @@ module.exports = {
         if(item?.itemComponent?.buyPrice == null){
             item.itemComponent.buyPrice = 0
         }
+        if(item?.itemInfo?.name == undefined){
+            item.itemInfo.name = "None"
+        }
+        if(item?.itemInfo?.description == undefined || item?.itemInfo?.description == ``){
+            item.itemInfo.description = "None"
+        }
+        if(item?.itemInfo?.internalNotes == undefined){
+            item.itemInfo.internalNotes = "None"
+        }
+        // console.log(item.itemInfo.name)
+        // console.log(item.itemInfo.description)
+        // console.log(item.itemInfo.internalNotes)
 
+        for(let skill in Object.keys(item.objectSkills)){
+
+            if(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.name && item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.damageCombo && item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.Description){
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.name)
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.damageCombo)
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.Description)
+            }
+        }
 
         let msgEmbed = require(`./embedTemplate.js`)
         if(item?.equipLocationNames?.length === 1){
@@ -54,10 +74,112 @@ module.exports = {
         let embed = msgEmbed.execute(`${item.itemInfo.displayName} [${item.objectID}]`, description, `https://lu-explorer.web.app/objects/${item.objectID}`, item.iconURL)
 
         embed.addFields(
-            { name: 'Name', value: item?.itemInfo?.name, inline: true },
-            { name: 'Description', value: item?.itemInfo?.description, inline: true },
-            { name: 'Internal Notes', value: item?.itemInfo?.internalNotes, inline: true },
+            { name: 'Name', value: item.itemInfo.name, inline: true },
+            { name: 'Description', value: item.itemInfo.description, inline: true },
+            { name: 'Internal Notes', value: item.itemInfo.internalNotes, inline: true },
         )
+        for(let skill in Object.keys(item.objectSkills)){
+
+            if(item.itemComponent.equipLocation[0] !== 'special_r' && item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.name && item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.damageCombo && item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.Description){
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.name)
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.damageCombo)
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.Description)
+                embed.addFields(
+                    {name: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.name, value: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.Description, inline: false},
+                )
+                embed.addFields(
+                    {name: "Imagination Cost", value: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.imaginationcost, inline: true},
+                    {name: "Cooldown", value: `${item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.cooldown} Seconds`, inline: true},
+                    {name: "Cooldown Group", value: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.cooldowngroup, inline: true},
+                )
+            }
+            else if(item.itemComponent.equipLocation[0] !== 'special_r' && item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info){
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.name)
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.damageCombo)
+                // console.log(item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.Description)
+                embed.addFields(
+                    {name: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.name, value: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.rawDescription, inline: false},
+                )
+                embed.addFields(
+                    {name: "Imagination Cost", value: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.imaginationcost, inline: true},
+                    {name: "Cooldown", value: `${item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.cooldown} Seconds`, inline: true},
+                    {name: "Cooldown Group", value: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.cooldowngroup, inline: true},
+                )
+            }
+            if(item.itemComponent.equipLocation[0] === 'special_r' && item['objectSkills'][Object.keys(item.objectSkills)[skill]]['castOnType'] === 0){
+                console.log(item['overview'][item['objectSkills'][Object.keys(item.objectSkills)[skill]]['behaviorID']])
+                if(item['overview'][item['objectSkills'][Object.keys(item.objectSkills)[skill]]['behaviorID']]?.damageComboArray) {
+                    embed.addFields(
+                        {
+                            name: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.name,
+                            value: item['overview'][item['objectSkills'][Object.keys(item.objectSkills)[skill]]['behaviorID']]?.damageComboArray.join('+'),
+                            inline: true
+                        },
+                        {
+                            name: "Single Jump Smash",
+                            value: item['overview'][item['objectSkills'][Object.keys(item.objectSkills)[skill]]['behaviorID']]?.singleJumpSmash,
+                            inline: true
+                        },
+                        {
+                            name: "Double Jump Smash",
+                            value: item['overview'][item['objectSkills'][Object.keys(item.objectSkills)[skill]]['behaviorID']]?.doubleJumpSmash,
+                            inline: true
+                        },
+                    )
+                }
+
+                if(item['overview'][item['objectSkills'][Object.keys(item.objectSkills)[skill]]['behaviorID']]?.hasChargeUp) {
+                    embed.addFields(
+                        {
+                            name: "Charge Up",
+                            value: item['objectSkills'][Object.keys(item.objectSkills)[skill]]?.info?.ChargeUp,
+                            inline: true
+                        },
+                        {
+                            name: "Charge Up Cost",
+                            value: Math.abs(item['overview'][item['objectSkills'][Object.keys(item.objectSkills)[skill]]['behaviorID']]?.chargeUpCost),
+                            inline: true
+                        },
+                        {
+                            name: "Extra",
+                            value: item['overview'][item['objectSkills'][Object.keys(item.objectSkills)[skill]]['behaviorID']]?.chargeUpCombo,
+                            inline: true
+                        },
+                    )
+                }
+
+
+            }
+        }
+        for(let skill in Object.keys(item.proxySkills)){
+            // console.log()
+            if(item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.name && item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.damageCombo && item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.Description){
+                // console.log(item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.name)
+                // console.log(item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.damageCombo)
+                // console.log(item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.Description)
+                embed.addFields(
+                    {name: item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.name, value: item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.Description, inline: false},
+                )
+                embed.addFields(
+                    {name: "Imagination Cost", value: item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.imaginationcost, inline: true},
+                    {name: "Cooldown", value: `${item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.cooldown} Seconds`, inline: true},
+                    {name: "Cooldown Group", value: item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.cooldowngroup, inline: true},
+                )
+            }
+            else if(item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info){
+                // console.log(item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.name)
+                // console.log(item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.damageCombo)
+                // console.log(item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.Description)
+                embed.addFields(
+                    {name: item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.name, value: item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.info?.rawDescription, inline: false},
+                )
+                embed.addFields(
+                    {name: "Imagination Cost", value: item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.imaginationcost, inline: true},
+                    {name: "Cooldown", value: `${item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.cooldown} Seconds`, inline: true},
+                    {name: "Cooldown Group", value: item['proxySkills'][Object.keys(item.proxySkills)[skill]]?.cooldowngroup, inline: true},
+                )
+            }
+        }
 
 
         if(item.isWeapon === false && item.abilityName !== undefined && item.equipLocation[0] !== "chest" && item.equipLocation[0] !== "legs" && item.localeDescription !== undefined){
@@ -75,8 +197,8 @@ module.exports = {
             )
         }
         embed.addFields(
-            { name: `${emojis.armor} Armor`, value: item?.stats?.lifeBonusUI, inline: true },
-            { name: `${emojis.heart} Health`, value: item?.stats?.armorBonusUI, inline: true },
+            { name: `${emojis.armor} Armor`, value: item?.stats?.armorBonusUI, inline: true },
+            { name: `${emojis.heart} Health`, value: item?.stats?.lifeBonusUI, inline: true },
             { name: `${emojis.imagination} Imagination`, value: item?.stats?.imBonusUI, inline: true },
         )
 
