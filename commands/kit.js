3b5263087e828520409efb3182373750b6e780fc
MasterTemple <dgmastertemple@protonmail.com>
2021-04-22 02:03:44 -0700

    skill bonuses

--- commands/kit.js
+++ commands/kit.js
@@ -32,34 +32,15 @@ module.exports = {
         var kitFile = require(`./../output/kitData/${kitID}.json`)
 
         let msgEmbed = require(`./../functions/embedTemplate.js`)
-        let uIcon = "https://images-ext-1.discordapp.net/external/yeozIqZ6L5llPU2kUINa2Y5agdt4reO0KN1Q1YAjAOQ/%3Fcb%3D20121121213649/https/static.wikia.nocookie.net/legomessageboards/images/c/ce/LU2.png/revision/latest"
 
         let embed = msgEmbed.execute(kitFile.name, undefined,`https://lu-explorer.web.app/objects/item-sets/${kitFile.id}`, kitFile.iconURL)
 
+        Object.keys(kitFile.skillSetDescriptions).forEach(function(element, key, _array){
+            embed.addField(`${element[element.length-1]} Piece Bonus`, kitFile.skillSetDescriptions[element], false)
+        })
 
 
-        // if(Object.keys(kitFile?.skillIDs)?.length > 0){
-        //     for (let e = 0; e < Object.keys(kitFile?.skillIDs).length; e++) {
-        //         let kitInfo = `Imagination Cost: **${kitFile.skillIDs[Object.keys(kitFile?.skillIDs)[e]].imaginationCost}**\nCooldown Time: **${kitFile.skillIDs[Object.keys(kitFile?.skillIDs)[e]].cooldownTime}**\n`
-        //         //console.log(e)
-        //         try {
-        //             for (let j = 0; j < Object.keys(kitFile.skillIDs[Object.keys(kitFile?.skillIDs)[e]].items).length; j++) {
-        //                 //console.log(kitFile.skillIDs[Object.keys(kitFile?.skillIDs)[e]])
-        //                 kitInfo = `${kitInfo}__${kitFile.skillIDs[Object.keys(kitFile?.skillIDs)[e]]['items'][Object.keys(kitFile.skillIDs[Object.keys(kitFile?.skillIDs)[e]].items)[j]].displayName}__ [${Object.keys(kitFile.skillIDs[Object.keys(kitFile?.skillIDs)[e]].items)[j]}]\n`
-        //             }
-        //             if(Object.keys(kitFile.skillIDs[Object.keys(kitFile?.skillIDs)[e]].items).length > 0) {
-        //                 embed.addField(`Skill ID: ${Object.keys(kitFile?.skillIDs)[e]}`, kitInfo, true)
-        //             }
-        //
-        //         }catch{
-        //             //console.log(`e`)
-        //
-        //         }
-        //         //console.log(kitInfo)
-        //
-        //
-        //     }
-        // }
+
         if(Object.keys(kitFile.totalWithoutValiant).length !== 0 && Object.keys(kitFile.totalWithValiant).length === 0) {
             //embed.addField("Total", "With Valiant")
             embed.addFields(
